91416517f65fa06ccd7aa290afadae2fc4c8eebb
Adithya Challa <76523584+Adi16023@users.noreply.github.com>
2023-10-03 12:47:24 +0530

    Changes 2.0

--- components/form-rsc.tsx
+++ components/form-rsc.tsx
@@ -42,7 +42,7 @@ export default function FormRSC({
                 alt="Logo image of FlipKart"
                 width="250"
                 height="250"
-                classname="mi-auto"
+                classname="mx-auto"
               ></Image>
       <p
         className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
